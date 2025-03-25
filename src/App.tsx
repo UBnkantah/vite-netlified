aebff6e86f41744547e8257d3724495f383d6d1c
Ubong Nkantah <ubongnkantahuwem@gmail.com>
2025-03-25 02:51:38 +0000

    first comit

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,25 @@ function App() {
       <div>
         <h2 className="6xl font-[600] text-center ">Contact Form</h2>
         <div className="w-full md:w-1/2 rounded-lg shadow mx-auto">
-        <form action="#" className="mx-auto ">
+        <form action="#" className="mx-auto " method="POST" data-netlify="true" name="vite-form">
           <div className="flex flex-col gap-1 items-start">
-            <label htmlFor="">First Name</label>
+            <label>First Name</label>
             <input type="text" name="firstname" id="firstname" placeholder="Enter your first name" />
           </div>
           <div className="flex flex-col gap-1 items-start">
-            <label htmlFor="">Last Name</label>
+            <label>Last Name</label>
             <input type="text" name="firstname" id="lastname" placeholder="Enter your last name" />
           </div>
           <div className="flex flex-col gap-1 items-start">
-            <label htmlFor="">Email Address</label>
+            <label>Email Address</label>
             <input type="email" name="email" id="email" placeholder="Enter your email address"/>
           </div>
           <div className="flex flex-col gap-1 items-start">
-            <label htmlFor="">Subject</label>
+            <label>Subject</label>
             <input type="text" name="subject" id="subject" placeholder="Enter subject"/>
           </div>
           <div className="flex flex-col gap-1 items-start">
-            <label htmlFor="">Message</label>
+            <label>Message</label>
             <textarea name="message" id="message" placeholder="Message" className="border border-neutral-100"></textarea>
           </div>
           <div>
